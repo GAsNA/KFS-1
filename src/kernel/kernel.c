566da77a88751c741515824fcd1bda9b308e289d
GAsNa <romane.leseur.pro@gmail.com>
2025-01-09 17:15:26 +0100

    insert screens var in terminal + fonction to update buff

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,15 +1,31 @@
 #include "kernel.h"
 
-t_terminal terminal =	{
-				.vidptr = (char*)0xb8000,
-				.current_loc = 0,
-				.shift = 0,
-				.capslock = 0,
-				.numslock = 0,
-			};
+t_terminal terminal;
+
+static t_screen init_screen(void)
+{
+	t_screen screen;
+
+	screen.current_loc = 0;
+	
+	return screen;
+}
+
+static void init_terminal(void)
+{
+	terminal.vidptr = (char*)0xb8000;
+	terminal.current_screen = 0;
+	terminal.screens[0] = init_screen();
+	terminal.current_loc = 0;
+	terminal.shift = 0;
+	terminal.capslock = 0;
+	terminal.numslock = 0;
+}
 
 void main(void)
 {
+	init_terminal();
+
 	clear_console();
 
 	printk("42 - rleseur", LIGHT_BLUE);
--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -5,9 +5,19 @@
 # include "keyboard.h"
 # include "console.h"
 
+# define LIMIT_NB_SCREENS 5
+
+typedef struct s_screen
+{
+	char	*buffer;
+	int	current_loc;
+} t_screen;
+
 typedef struct s_terminal
 {
 	char		*vidptr;
+	int		current_screen;
+	t_screen	screens[LIMIT_NB_SCREENS];
 	int		current_loc;
 	int		shift;
 	int		capslock;
