ec45865bfba635ac6473ccab875647fdc7d1d870
GAsNa <romane.leseur.pro@gmail.com>
2025-01-12 00:24:36 +0100

    define SCREENSIZE in kernel.h

--- src/kernel/console.h
+++ src/kernel/console.h
@@ -1,13 +1,6 @@
 #ifndef CONSOLE_H
 # define CONSOLE_H
 
-# define NB_LINES 25
-# define NB_COLUMNS 80
-# define BYTES_FOR_ELEMENT 2
-# define SCREENSIZE BYTES_FOR_ELEMENT * NB_COLUMNS * NB_LINES
-
-# define TAB_SIZE 4
-
 void clear_console(void);
 void newline_on_console(void);
 void delete_on_console(void);
