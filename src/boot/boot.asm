1b06b3123ae25115a3299ec9f28be8bf95a6503a
GAsNa <romane.leseur.pro@gmail.com>
2025-01-22 23:42:48 +0100

    add function to move cursor on position (not use yet)

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -11,6 +11,7 @@ section .text
 
 global read_port
 global write_port
+global outb
 global start
 
 extern main				;main is defined in the c file
@@ -25,6 +26,16 @@ write_port:
 	mov al, [esp + 4 + 4]
 	out dx, al
 	ret
+
+; outb - send a byte to an I/O port
+; stack: [esp + 8] the data byte
+;        [esp + 4] the I/O port
+;        [esp    ] return address
+outb:
+    mov al, [esp + 8]    ; move the data to be sent into the al register
+    mov dx, [esp + 4]    ; move the address of the I/O port into the dx register
+    out dx, al           ; send the data to the I/O port
+    ret                  ; return to the calling function
 
 start:
 	cli				;block interrupts
