120279492d3eb2776bfd3c7f23c7976b92be86c4
GAsNa <romane.leseur.pro@gmail.com>
2025-01-23 17:43:52 +0100

    reorga of keyboard file

--- src/kernel/keyboard.c
+++ src/kernel/keyboard.c
@@ -39,6 +39,70 @@ void simulate_keyboard_interrupt(void)
 }
 
 /**
+ * Check if the keycode corresponds to a shortcut or other non printable character
+ *
+ * @return true if the keycode corresponds to one of this non printable character
+ */
+static int check_for_non_printable_keycode(unsigned char keycode)
+{
+	int is_checked = 0;
+
+	if (keycode == CAPSLOCK)
+	{
+		terminal.capslock = !terminal.capslock;
+		is_checked = 1;
+	}
+	else if (keycode == NUMSLOCK)
+	{
+		terminal.numslock = !terminal.numslock;
+		is_checked = 1;
+	}
+	else if (keycode == F1)
+	{
+		change_screen(terminal.current_screen - 1);
+		is_checked = 1;
+	}
+	else if (keycode == F2)
+	{
+		change_screen(terminal.current_screen + 1);
+		is_checked = 1;
+	}
+	// If is left arrow or keypad 4 with no numslock, move cursor to left
+	else if (keycode == KEYPAD4 && (terminal.to_escape == 1 || terminal.numslock == 0))
+	{
+		terminal.current_loc -= 2;
+		move_cursor(terminal.current_loc / 2);
+		is_checked = 1;
+	}
+	// If is right arrow or keypad 6 with no numslock, move cursor to right
+	else if (keycode == KEYPAD6 && (terminal.to_escape == 1 || terminal.numslock == 0))
+	{
+		terminal.current_loc += 2;
+		move_cursor(terminal.current_loc / 2);
+		is_checked = 1;
+	}
+	// If is up arrow or keypad 8 with no numslock, move cursor up
+	else if (keycode == KEYPAD8 && (terminal.to_escape == 1 || terminal.numslock == 0))
+	{
+		terminal.current_loc -= NB_COLUMNS * 2;
+		move_cursor(terminal.current_loc / 2);
+		is_checked = 1;
+	}
+	// If is down arrow or keypad 2 with no numslock, move cursor down
+	else if (keycode == KEYPAD2 && (terminal.to_escape == 1 || terminal.numslock == 0))
+	{
+		terminal.current_loc += NB_COLUMNS * 2;
+		move_cursor(terminal.current_loc / 2);
+		is_checked = 1;
+	}
+
+	// All escaped keys have been checked
+	terminal.to_escape = 0;
+
+	return is_checked;
+}
+
+/**
  * Get a keycode from the port 0x60 and print on terminal the associated char
  *
  * @return void
@@ -57,30 +121,14 @@ void keyboard_handler(void) {
 
 	keycode = read_port(KEYBOARD_DATA_PORT);
 
+	if (keycode < 0)
+		return;
+	
 	/* Check if this is a key to escaped */
 	/* 0xe0 code is sent before the actual code if it's a key that can be escaped */
 	if (keycode == ESCAPED_KEY_CODE)
 	{
 		terminal.to_escape = 1;
-		return;
-	}
-
-	if (terminal.to_escape == 1)
-	{
-		// TODO: All in move_cursor
-		if (keycode == KEYPAD4)
-			terminal.current_loc -= 2;
-		else if (keycode == KEYPAD6)
-			terminal.current_loc += 2;
-		else if (keycode == KEYPAD8)
-			terminal.current_loc -= NB_COLUMNS * 2;
-		else if (keycode == KEYPAD2)
-			terminal.current_loc += NB_COLUMNS * 2;
-
-		//Update cursor
-		move_cursor(terminal.current_loc / 2);
-
-		terminal.to_escape = 0;
 		return;
 	}
 
@@ -91,9 +139,11 @@ void keyboard_handler(void) {
 	if (!is_pressed)
 		keycode -= RELEASED_KEY_CODE;
 
-	/* Check if keycode is valid and is shift */
-	if (keycode < 0)
-		return;
+	/* If not pressed and is escaped, then is not escaped */
+	if (!is_pressed && terminal.to_escape)
+		terminal.to_escape = 0;
+		
+	/* Check if keycode is shift */
 	if (keycode == LSHIFT || keycode == RSHIFT)
 	{
 		if (is_pressed)
@@ -106,29 +156,11 @@ void keyboard_handler(void) {
 	/* If key is not pressed, do not write on terminal and return */
 	if (!is_pressed)
 		return;
-		
-	/* Check the keycode for non-printable char */
-	if (keycode == CAPSLOCK)
-	{
-		terminal.capslock = !terminal.capslock;
-		return;
-	}
-	if (keycode == NUMSLOCK)
-	{
-		terminal.numslock = !terminal.numslock;
-		return;
-	}
-	if (keycode == F1)
-	{
-		change_screen(terminal.current_screen - 1);
-		return;
-	}
-	if (keycode == F2)
-	{
-		change_screen(terminal.current_screen + 1);
-		return;
-	}
 
+	/* Check the keycode for non-printable char */
+	if (check_for_non_printable_keycode(keycode))
+		return;
+		
 	/* Selected the char given by inputs */
 	selected_key = keyboard_map[keycode];
 
@@ -140,19 +172,9 @@ void keyboard_handler(void) {
 			return;
 	}
 	else if (terminal.capslock && selected_key.capital != 0)
-	{
-		if (terminal.shift)
-			selected_char = selected_key.small;
-		else
-			selected_char = selected_key.capital;
-	}
+		selected_char = terminal.shift ? selected_key.small : selected_key.capital;
 	else if (selected_key.small != 0)
-	{
-		if (terminal.shift)
-			selected_char = selected_key.capital;
-		else
-			selected_char = selected_key.small;
-	}
+		selected_char = terminal.shift ? selected_key.capital : selected_key.small;
 	else
 		return;
 
